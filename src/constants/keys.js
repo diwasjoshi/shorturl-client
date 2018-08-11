0f1908b1740b4589c2ff9c7c7098ac6a7c891f44
diwasjoshi <diwas.joshi2@gmail.com>
2018-08-11 11:41:38 +0000

    correct environment vars

--- src/constants/keys.js
+++ src/constants/keys.js
@@ -1,1 +1,5 @@
- export const SHORT_URLS_HOST =  process.env.SHORT_URLS_HOST
+import runtimeEnv from '@mars/heroku-js-runtime-env';
+
+const env = runtimeEnv();
+
+export const SHORT_URLS_HOST = env.REACT_APP_SHORT_URLS_HOST
--- src/constants/serviceUrls.js
+++ src/constants/serviceUrls.js
@@ -1,1 +1,6 @@
-export const BASE_URL = process.env.BASE_URL
+import runtimeEnv from '@mars/heroku-js-runtime-env';
+
+const env = runtimeEnv();
+
+
+export const BASE_URL = env.REACT_APP_BASE_URL
